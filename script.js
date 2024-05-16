70badd7e48d093d3642d913fcfb23ccf2add0b26
untilBrian <brianyu267@gmail.com>
2024-05-16 18:09:46 -0400

    finished the GUI

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-//Gameboard object
-    //add move (check if space is taken though)
-    //calculate win condition
-        //perform check after each round
-    //reset board
-    //player.Move()?
-
-
 function createGameboard() {
     let gameboard = {
         //variables
@@ -15,15 +7,17 @@ function createGameboard() {
             [0, 3, 6], [1, 4, 7], [2, 5, 8],
             [0, 4, 8], [2, 4, 6],
         ],
+        currentPlayer: null, 
+        p1: null,
+        p2: null,
 
         //methods
         addMove: function(currentPlayerHolder, player, location, move) {
-            if (this.checkMoveFeasibility(location-1)) {
-                this.gameboardArray[location-1] = move + ": player" + player;
+            if (this.checkMoveFeasibility(location)) {
+                this.gameboardArray[location] = move + ": player" + player;
             }
             else { 
                 console.log("Please try again. That spot is taken.")
-                this.addMove(currentPlayerHolder, player, currentPlayerHolder.move(), move);
             }
         },
 
@@ -40,39 +34,81 @@ function createGameboard() {
                 if(this.gameboardArray[combo[0]] !== "" 
                 && this.gameboardArray[combo[0]] === this.gameboardArray[combo[1]] 
                 && this.gameboardArray[combo[1]] === this.gameboardArray[combo[2]]) {
-                this.assignWinner(this.gameboardArray[combo[0]])
-                return true; 
+                    return true; 
                 }
             }
             return false;
         },
 
-        assignWinner: function(moveInfo) {
-            let lastChar = moveInfo[moveInfo.length - 1]
-            return lastChar; 
+
+        startGame: function(p1, p2) {
+            this.p1 = p1; 
+            this.p2 = p2; 
+            this.currentPlayer = this.p1; //p1 is the player Object
+            this.displayBoard(); 
+            },
+
+        endGame: function() {
+            document.body.removeChild(visualBoard);
+            this.gameboardArray = ["", "", "", "", "", "", "", "", ""]
+            alert(this.currentPlayer.name + " is the winner!")
+            this.startGame(this.p1, this.p2);
         },
 
-        playGame: function(p1, p2) {
-            let currentPlayer = p1; 
+        resetGame: function() {
+            document.body.removeChild(visualBoard);
+            this.gameboardArray = ["", "", "", "", "", "", "", "", ""];
+            this.startGame(this.p1, this.p2);
+        },
+        
+        processMove: function(num) {
+            this.addMove(this.currentPlayer, this.currentPlayer.whichPlayer, num, this.currentPlayer.shape);
+            this.updateBoard(num, this.currentPlayer.shape);
 
-            //switching players
-            for (var i = 1; ; i++) {
-                currentPlayer = i % 2 === 0 ? p2 : p1; 
-                this.addMove(currentPlayer, currentPlayer.whichPlayer, currentPlayer.move(), currentPlayer.shape)
-                console.log(this.gameboardArray);
-                
-                if (this.checkWinCondition()) {
-                    console.log("Player " + currentPlayer.whichPlayer + " wins!" + " That's also " + currentPlayer.name)
-                    this.gameboardArray = ["", "", "", "", "", "", "", "", ""];
-
-                    break; 
-                }
-
-                if (!confirm("Continue to next move?")) {
-                    break; 
-                }
+            if (this.checkWinCondition()) {
+                this.endGame();
+            } else {
+                this.switchPlayers();
             }
-        }   
+        },
+
+        switchPlayers: function() {
+            if (this.currentPlayer === this.p1) {
+                this.currentPlayer = this.p2; 
+            }
+            else {
+                this.currentPlayer = this.p1;
+            }
+        },
+
+        updateBoard: function(num, shape) {
+            let box = document.querySelector("#box" + num);
+
+            if (shape === "cross") {
+                box.textContent = "X";
+            } else if (shape === "circle") {
+                box.textContent = "O";
+            }
+        },
+    
+        displayBoard: function() {
+            const visualBoard = document.createElement("div"); 
+            visualBoard.id = "visualBoard";
+            let boxes = [];
+
+            for (let i = 0; i < 9; i++) {
+                boxes[i] = document.createElement("div");
+                boxes[i].id = "box" + i;
+                boxes[i].classList.add("box");
+
+                boxes[i].addEventListener("click", () => {
+                    this.processMove(i);
+                });
+
+                visualBoard.appendChild(boxes[i]); 
+                document.body.appendChild(visualBoard);              
+            }
+        }
     }
 
     return gameboard; 
@@ -84,12 +120,7 @@ function createPlayer(name, whichPlayer) {
         whichPlayer: whichPlayer, 
         name: name, 
         shape: "cross",
-
-        move: function() {
-            let loc = prompt(player.name + ", What is your location?")
-            return loc; 
         }
-    }
 
     if (player.whichPlayer === 2) {
         player.shape = "circle"; 
@@ -99,7 +130,27 @@ function createPlayer(name, whichPlayer) {
 
 }
 
-let littleBrian = createPlayer ("lilBrian", 1);
-let bigBrian = createPlayer ("bigBoy", 2);
-let testGame = createGameboard();
-testGame.playGame(littleBrian, bigBrian)
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", () => {
+    game.resetGame();
+});
+
+let p1 = createPlayer ("Player 1", 1);
+let p2 = createPlayer ("Player 2", 2);
+let game = createGameboard();
+game.startGame(p1, p2);
+
+
+
+//add eventListener to boxes
+
+//playGame triggers -> boxes are displayed with eventListener
+//eventListener triggers addMove when clicked - don't need move() anymore?
+//r1: box A is now filled (i.e. the array)
+//how to switch players? while loop () => {switch?}
+ //r2: box B is now filled
+
+//how to tell when the game is finished? (if winCondition == true
+
+//playGame makes changes
+//playGame makes changes
